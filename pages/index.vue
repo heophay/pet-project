4c575104371994c9123f94c1926231de4a191da1
Rôn <heophay0509@gmail.com>
2023-03-24 23:30:40 +0700

    feat(ol): Init project

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Tutorial />
+    <h1 :class="$style.huy"> Huy Day </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style lang="scss" module>
+.huy {
+  color: red;
+}
+</style>
